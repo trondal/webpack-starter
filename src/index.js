7c17a204af8544094046d1f807b4ea98b5e68bce
Trond Albinussen <trond.albinussen@blinq.no>
2022-08-12 09:17:52 +0200

    Add Babel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import generateJoke from "./generateJoke";
 import "./styles/main.css";
+
 console.log(generateJoke());
 console.log(1);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   mode: "development",
@@ -10,6 +12,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
     clean: true,
+    assetModuleFilename: "[name][ext]", // keep img original name
   },
   devtool: "source-map",
   devServer: {
@@ -28,13 +31,31 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i, // test alle disse
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: "webpack-starter",
       filename: "index.html",
-      template: path.resolve(__dirname, "src/index.html"),
+      template: path.resolve(__dirname, "public/index.html"),
+      favicon: "./public/fav_32.png",
     }),
+    new CopyPlugin({ patterns: [{ from: "./public/manifest.json", to: "." }] }),
+    new CopyPlugin({ patterns: [{ from: "./public/fav_152.png", to: "." }] }),
+    // new BundleAnalyzerPlugin(), // Enable to view visual bundle analyzer
   ],
 };
